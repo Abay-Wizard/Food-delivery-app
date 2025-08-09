74674f047175245649e06a4779710d0f1ab66749
Abay-Wizard <abaytessema251@gmail.com>
2025-08-09 14:09:43 +0300

    frontend updated

--- frontend/app.js
+++ frontend/app.js
@@ -1,1 +1,6 @@
 import React from 'react'
+// firstly i initialize git
+// then i add remote rep url from github
+// then i add my dir => git add .
+// and then i commit => git commit -m "some message related to the commit"
+// finally i push it to github => git push -u origin main
